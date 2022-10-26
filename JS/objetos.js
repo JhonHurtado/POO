c5ca3a67f06b2f31cc0a55e02253787eb47e7ebb
Jhon Hurtado <116118632+JhonHurtado@users.noreply.github.com>
2022-10-26 11:21:24 -0500

    objetos

--- JS/objetos.js
+++ JS/objetos.js
@@ -1,5 +1,22 @@
-'user strict';
+
 class misObjetos{
-    
-    
-};
+    objeto1=new Object();
+    objeto2={};
+
+
+    objpaz={
+        indentificacion:0,
+        nombre:" ",
+        estado:" ",
+        actividad:" ",
+        juego:" "
+    };
+
+    datospaz(id,nom,estado,actividad,juego) {
+        alert(id+" "+nom+" "+estado+" "+actividad+" "+juego);
+    };
+
+}
+
+
+var instMisObjetos = new misObjetos();
